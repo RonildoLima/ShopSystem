b6d2a5271cc45cc8a81e25ff856c0c1d5a0ec1f5
Ronildo <ronildolima996@gmail.com>
2025-01-26 18:31:48 -0300

    Repository PedidoHistoricoStatus actualized

--- src/main/java/com/accenture/shopsystem/repositories/PedidoHistoricoStatusRepository.java
+++ src/main/java/com/accenture/shopsystem/repositories/PedidoHistoricoStatusRepository.java
@@ -1,4 +1,5 @@
 package com.accenture.shopsystem.repositories;
+import com.accenture.shopsystem.domain.Pedido.Pedido;
 import com.accenture.shopsystem.domain.PedidoHistoricoStatus.PedidoHistoricoStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -11,4 +12,6 @@ import java.util.List;
 public interface PedidoHistoricoStatusRepository extends JpaRepository<PedidoHistoricoStatus, String> {
     @Query("SELECT h FROM PedidoHistoricoStatus h WHERE h.pedido.vendedor.id = :vendedorId")
     List<PedidoHistoricoStatus> findByPedidoVendedorId(@Param("vendedorId") String vendedorId);
+
+    void deleteByPedido(Pedido pedido);
 }
